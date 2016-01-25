1bd3389793ab6fb156c1ea579db6b48571e91b10
Pranav Ganorkar <pranavg189@gmail.com>
2016-01-25 14:31:47 +0530

    Added ptyFork() and ttyReset()

--- miniterm.c
+++ miniterm.c
@@ -7,9 +7,9 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
-#include <termios.h> /* unix terminal API */
-#include <sys/ioctl.h>
-#include <sys/types.h>
+#include <termios.h> /* Unix terminal API */
+#include <sys/ioctl.h> /* required for winsize struct and ioctl() */
+#include <sys/types.h> /* required for pid_t datatype */
 #include <sys/select.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -32,6 +32,88 @@ int min_cursor_offset = 0;
 
 GThread *thread;
 
+char slaveName[MAX_SNAME];
+char *shell;
+int masterFd, scriptFd;
+struct winsize ws;		/* winsize is a data structure defined in sys/ioctl.h and used to store info about window size */
+fd_set inFds;			/* fd_set is a fixed size buffer */
+char buf[BUF_SIZE];
+ssize_t numRead; 		/* ssize_t data type is used to represent the sizes of block that can be read or written in a single operation. */
+pid_t childPid; 		/* pid_t data type represents process IDs */
+
+struct termios ttyOrig;
+
+pid_t ptyFork(int *masterFd, char *slaveName, size_t snLen, struct termios *slaveTermios, const struct winsize *slaveWS)
+{
+	int mfd, slaveFd, savedErrno;
+	pid_t childPid;
+	char slname[100];
+	char *p;
+	mfd = posix_openpt(O_RDWR | O_NOCTTY); /* creates a new pseudo terminal */
+	grantpt(mfd);  /* grant required permissions */
+	unlockpt(mfd);
+	p = ptsname(mfd); /* get pseudo terminal name */
+	strncpy(slname, p, 100);
+	if (slaveName != NULL)
+	{
+		/* Return slave name to caller */
+		if(strlen(slname) < snLen)
+		{
+			strncpy(slaveName,slname,snLen);
+		}
+	}
+	childPid = fork();
+	if(childPid != 0)
+	{
+		*masterFd = mfd;
+		return childPid;
+	}
+	setsid();
+	close(mfd);
+	slaveFd = open(slname, O_RDWR);
+	#ifdef TIOCSCTTY
+	ioctl(slaveFd, TIOCSCTTY, 0);
+	#endif
+	if(slaveTermios != NULL)
+	{
+		slaveTermios->c_lflag &= ~ECHO;
+		slaveTermios->c_lflag |= ECHOE;
+		slaveTermios->c_lflag |= ECHOK;
+		//slaveTermios->c_lflag |= ECHOKE;
+		//slaveTermios->c_lflag |= ECHOCTL;
+		slaveTermios->c_lflag |= ICANON;
+		slaveTermios->c_lflag |= ISIG;
+		slaveTermios->c_lflag |= IEXTEN;
+		slaveTermios->c_lflag |= HUPCL;
+		slaveTermios->c_iflag |= BRKINT;
+		slaveTermios->c_iflag |= ICRNL;
+		slaveTermios->c_iflag |= IXON;
+		slaveTermios->c_iflag |= IXANY;
+		slaveTermios->c_iflag |= IUTF8;
+		slaveTermios->c_iflag |= IMAXBEL;
+		slaveTermios->c_oflag |= OPOST;
+		slaveTermios->c_oflag |= ONLCR;
+		cfsetspeed(slaveTermios, B38400);
+		slaveTermios->c_cc[VMIN]=1;
+		if (tcsetattr(slaveFd, TCSANOW, slaveTermios) == -1);
+	}
+
+	if(slaveWS != NULL)
+		if(ioctl(slaveFd, TIOCSWINSZ, slaveWS) == -1);
+
+	dup2(slaveFd, 0);
+	dup2(slaveFd, 1);
+	dup2(slaveFd, 2);
+
+	if(slaveFd > 2)
+		close(slaveFd);
+	return 0;
+}
+
+static void ttyReset(void)
+{
+	if(tcsetattr(STDIN_FILENO), TCSANOW, &ttyOrig) == -1);
+}
 
 int main()
 {
